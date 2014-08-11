d99f37d372b3f49040a72cf64fce8ea3579e5f71
suisho <suisho@suisho-no-MacBook-Air.local>
2014-08-12 00:00:44 +0900

    initial commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by suisho on 2014/08/12.
+//  Copyright (c) 2014年 suisho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

a91f8d9033960a02d36bfdf64fe8b951390faf8f
suisho <suisho000000@gmail.com>
2014-08-13 00:01:14 +0900

    daily forcast

--- SimpleWeather/WXDailyForecast.m
+++ SimpleWeather/WXDailyForecast.m
@@ -10,4 +10,13 @@
 
 @implementation WXDailyForecast
 
++ (NSDictionary *)JSONKeyPathsByPropertyKey{
+    NSMutableDictionary *paths = [[super JSONKeyPathsByPropertyKey] mutableCopy];
+    
+    paths[@"tempHigh"] = @"temp.max";
+    paths[@"tempLow"] = @"temp.min";
+    
+    return paths;
+}
+
 @end

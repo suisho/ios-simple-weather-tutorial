078d441fdcd2432e5915be76a52a284e94830cce
suisho <suisho000000@gmail.com>
2014-08-12 20:31:10 +0900

    status barを白く

--- SimpleWeather/WXController.m
+++ SimpleWeather/WXController.m
@@ -35,7 +35,9 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
 /*
 #pragma mark - Navigation
 

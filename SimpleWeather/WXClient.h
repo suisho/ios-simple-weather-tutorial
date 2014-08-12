eb6ea15c52ead0076fb3698491a0566bb1c3a86a
suisho <suisho000000@gmail.com>
2014-08-13 00:09:53 +0900

    reactive

---
+++ SimpleWeather/WXClient.h
@@ -0,0 +1,22 @@
+//
+//  WXClient.h
+//  SimpleWeather
+//
+//  Created by suisho on 2014/08/12.
+//  Copyright (c) 2014年 suisho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+/*
+@import CoreLocation;
+#import <ReactiveCocoa/ReactiveCocoa/ReactiveCocoa.h>
+@import Foundation;
+*/
+@interface WXClient : NSObject
+/*
+- (RACSignal *)fetchJSONFromURL:(NSURL *)url;
+- (RACSignal *)fetchCurrentConditionsForLocation:(CLLocationCoordinate2D)cordinate;
+- (RACSignal *)fetchHourlyForecastForLocation:(CLLocationCoordinate2D)cordinate;
+- (RACSignal *)fetchDailyForecastForLocation:(CLLocationCoordinate2D)cordinate;
+*/
+@end
---
+++ SimpleWeather/WXClient.m
@@ -0,0 +1,13 @@
+//
+//  WXClient.m
+//  SimpleWeather
+//
+//  Created by suisho on 2014/08/12.
+//  Copyright (c) 2014年 suisho. All rights reserved.
+//
+
+#import "WXClient.h"
+
+@implementation WXClient
+
+@end

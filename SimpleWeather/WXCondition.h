14c1d0e8fe5871de7ad28006f67dd66a3aecedbd
suisho <suisho000000@gmail.com>
2014-08-12 23:17:24 +0900

    head

--- SimpleWeather/WXCondition.h
+++ SimpleWeather/WXCondition.h
@@ -7,7 +7,24 @@
 //
 
 #import "MTLModel.h"
+#import <Mantle.h>
 
-@interface WXCondition : MTLModel
+@interface WXCondition : MTLModel <MTLJSONSerializing>
+
+@property (nonatomic, strong) NSData *date;
+@property (nonatomic, strong) NSNumber *humidity;
+@property (nonatomic, strong) NSNumber *temperature;
+@property (nonatomic, strong) NSNumber *tempHigh;
+@property (nonatomic, strong) NSNumber *tempLow;
+@property (nonatomic, strong) NSString *locationName;
+@property (nonatomic, strong) NSData *sunrise;
+@property (nonatomic, strong) NSData *sunset;
+@property (nonatomic, strong) NSString *conditionDescription;
+@property (nonatomic, strong) NSString *condition;
+@property (nonatomic, strong) NSNumber *windBearing;
+@property (nonatomic, strong) NSNumber *windSpeed;
+@property (nonatomic, strong) NSString *icon;
+
+- (NSString *)imageName;
 
 @end

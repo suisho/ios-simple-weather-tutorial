6a0c8ac6a715835230f7a520b4ca5bf516294e3b
suisho <suisho000000@gmail.com>
2014-08-12 20:29:20 +0900

    赤くなる

---
+++ SimpleWeather/WXController.h
@@ -0,0 +1,13 @@
+//
+//  WXController.h
+//  SimpleWeather
+//
+//  Created by suisho on 2014/08/12.
+//  Copyright (c) 2014年 suisho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXController : UIViewController
+
+@end
